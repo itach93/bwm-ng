faa63ade75aa5574fea8adde2b2e0215aaada9f2
itachi <daitachi93@gmail.com>
2019-01-07 04:36:25 +0100

    add proxy configuration, adds renyals details styles

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import {NgPipesModule} from 'ngx-pipes';
 
 
 import { RentalListComponent } from './rental-list/rental-list.component';
@@ -8,6 +10,7 @@ import { RentalListItemComponent } from './rental-list-item/rental-list-item.com
 import { RentalComponent } from './rental.component';
 import { RentalService } from './shared/rental.service';
 import { RentailDetailComponent } from './rentail-detail/rentail-detail.component';
+import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 
 const routes: Routes = [
     {   path: 'rentals',
@@ -24,11 +27,14 @@ const routes: Routes = [
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent,
-        RentailDetailComponent
+        RentailDetailComponent,
+        UppercasePipe
     ],
     imports: [
         CommonModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        HttpClientModule,
+        NgPipesModule
     ],
     providers: [RentalService]
 })
